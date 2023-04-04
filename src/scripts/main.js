23e23a5fc0d67863585d35381e10cb6385aa057f
Elisafa <elisafajunior@hotmail.com>
2023-04-03 21:11:20 -0300

    Finalizado a sessão dos personagens

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
-    
+    const panels = document.querySelectorAll('.personagens__container__painel')
+
     buttons.forEach(button => {
         button.addEventListener('click', function(botao){
             const alvo = botao.target.dataset.tabButton
@@ -11,7 +12,13 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('historia__tabs__button--is-active')
         })
     })
-    
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', () => {
+            removeClasseActivePersonagens()
+            panel.classList.add('personagens__container-active')
+        })
+    })
 })
 
 function removeBotaoAtivo(){
@@ -23,10 +30,14 @@ function removeBotaoAtivo(){
 
 function escondeHistorias(){
     const historiaContainer = document.querySelectorAll('[data-tab-id]')
-    console.log(historiaContainer)
     historiaContainer.forEach(item => {
         item.classList.remove('historia__container--is-active')
     })
+}
 
-
+function removeClasseActivePersonagens(){
+    const panels = document.querySelectorAll('.personagens__container__painel')
+    panels.forEach(panel => {
+        panel.classList.remove('personagens__container-active')
+    })
 }
